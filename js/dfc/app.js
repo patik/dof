c435b222b95b57240067f71c9968809e1f61fa81
Craig Patik <craig@patik.com>
2014-03-23 13:57:18 -0400

    Added chart.js library and basic example

--- js/dfc/app.js
+++ js/dfc/app.js
@@ -17,7 +17,12 @@ var DFC = (function _DFC() {
         $comparisonLinks = null,
 
         // Sorting API
-        _sorting = {};
+        _sorting = {},
+
+        // Chart
+        _chart = {},
+        $chart = null,
+        dofChart = null;
 
     /**
      * Initialize app
@@ -85,7 +90,66 @@ var DFC = (function _DFC() {
             _addLensUI(lens);
             lens = _getNameFromUI(lens);
         });
+
+        $chart = $('#dofChart');
+        _chart.create();
     }
+
+    _chart.data = {
+        labels: ["5'", "10'", "15'", "20'", "25'", "30'", "35'", "40'", "45'", "50'"],
+        datasets: [
+            {
+                fillColor : "rgba(220,220,220,0.5)",
+                strokeColor : "rgba(220,220,220,1)",
+                pointColor : "rgba(220,220,220,1)",
+                pointStrokeColor : "#fff",
+                data : [5,10,15,20,25,30,35]
+            },
+            {
+                fillColor : "rgba(151,187,205,0.5)",
+                strokeColor : "rgba(151,187,205,1)",
+                pointColor : "rgba(151,187,205,1)",
+                pointStrokeColor : "#fff",
+                data : [5,15,25,35,45,55,65]
+            }
+        ]
+    };
+
+    _chart.options = {
+        scaleOverlay: false, //Boolean - If we show the scale above the chart data
+        scaleOverride: false, //Boolean - If we want to override with a hard coded scale
+        scaleSteps: null, //Number - The number of steps in a hard coded scale. ** Required if scaleOverride is true **
+        scaleStepWidth: null, //Number - The value jump in the hard coded scale
+        scaleStartValue: null, //Number - The scale starting value
+        scaleLineColor: 'rgba(0,0,0,.1)', //String - Colour of the scale line
+        scaleLineWidth: 1, //Number - Pixel width of the scale line
+        scaleShowLabels: true, //Boolean - Whether to show labels on the scale
+        scaleLabel: '<%=value%>', //Interpolated JS string - can access value
+        scaleFontSize: 12, //Number - Scale label font size in pixels
+        scaleFontStyle: 'normal', //String - Scale label font weight style
+        scaleFontColor: '#666',     //String - Scale label font colour
+        scaleShowGridLines: true, //Boolean - Whether grid lines are shown across the chart
+        scaleGridLineColor: 'rgba(0,0,0,.05)', //String - Colour of the grid lines
+        scaleGridLineWidth: 1,  //Number - Width of the grid lines
+        bezierCurve: true, //Boolean - Whether the line is curved between points
+        pointDot: true, //Boolean - Whether to show a dot for each point
+        pointDotRadius: 3, //Number - Radius of each point dot in pixels
+        pointDotStrokeWidth: 1, //Number - Pixel width of point dot stroke
+        datasetStroke: true, //Boolean - Whether to show a stroke for datasets
+        datasetStrokeWidth: 2, //Number - Pixel width of dataset stroke
+        animation: true, //Boolean - Whether to animate the chart
+        animationSteps: 60, //Number - Number of animation steps
+        animationEasing: 'easeOutQuart', //String - Animation easing effect
+        onAnimationComplete: null, //Function - Fires when the animation is complete
+
+        // Customized
+        scaleFontFamily: 'Open Sans', //String - Scale label font declaration for the scale label
+        datasetFill: false //Boolean - Whether to fill the dataset with a colour
+    };
+
+    _chart.create = function _chart_creat() {
+        dofChart = new Chart($chart.get(0).getContext('2d')).Line(_chart.data, _chart.options);
+    };
 
     // example.com/#20;Name%20of%20Lens,35,f-2,mft
     function _readLensesFromHash() {
