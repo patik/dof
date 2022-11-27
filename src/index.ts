0d783567cf19c70e5fd8de024ec1160d99b864a5
Craig Patik <craig@patik.com>
2022-11-27 16:02:22 +0100

    convert to class

--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,13 @@ type Result = {
     toString: () => string
 }
 
-type Lens = {
-    focalLength: number
-    aperture: number
-    cropFactor: number
-    name: string
-    id: string
-}
+// type Lens = {
+//     focalLength: number
+//     aperture: number
+//     cropFactor: number
+//     name: string
+//     id: string
+// }
 
 type Options = {
     focalLength: number
@@ -157,92 +157,82 @@ function _decimalAdjust(value: number): number {
  * Lens constructor
  *
  * @param  {Number}  focalLength  Actual local length in millimeters
- * @param  {Mixed}   aperture     Aperture as a float or a string like "f/2.5"
+ * @param  {Number | String}   aperture     Aperture as a float or a string like "f/2.5"
  * @param  {Number}  cropFactor   Sensor crop factor
  * @param  {Mixed}   id           Optional, arbitrary ID for tracking by the consumer
  * @param  {Mixed}   name         Optional, arbitrary name for tracking by the consumer
  */
-const DoF = function (
-    focalLength: number,
-    aperture: string | number,
-    cropFactor: number,
-    id: string | number,
-    name: string
-) {
-    const lens: Lens = {
-        focalLength: 0,
-        aperture: 0,
-        cropFactor: 0,
-        id: '',
-        name: '',
-    }
+class DoF {
+    focalLength: number
+    aperture: number
+    cropFactor: number
+    name = ''
+    id = ''
 
-    lens.focalLength = focalLength
+    constructor(focalLength: number, aperture: string | number, cropFactor: number, id: string | number, name: string) {
+        this.focalLength = focalLength
 
-    if (typeof aperture === 'number') {
-        lens.aperture = aperture
-    } else if (typeof aperture === 'string' && apertureRegex.test(aperture)) {
-        const match = apertureRegex.exec(aperture)
+        if (typeof aperture === 'number') {
+            this.aperture = aperture
+        } else if (typeof aperture === 'string' && apertureRegex.test(aperture)) {
+            const match = apertureRegex.exec(aperture)
 
-        lens.aperture = parseFloat(match ? match[1] : '0')
-    } else {
-        lens.aperture = defaults.aperture
-    }
-
-    if (typeof cropFactor === 'number') {
-        lens.cropFactor = cropFactor
-    } else {
-        lens.cropFactor = defaults.cropFactor
-    }
-
-    // Optional properties
-    if (typeof id === 'string') {
-        lens.id = id
-    }
-
-    if (typeof name === 'string') {
-        lens.name = name
-    }
-
-    this = lens
-}
-
-DoF.setDefaults = function _setDefaults(options: Options) {
-    if (typeof options !== 'object' || !options) {
-        return
-    }
-
-    if (options.focalLength && !isNaN(options.focalLength)) {
-        defaults.focalLength = options.focalLength
-    }
-
-    if (options.aperture && !isNaN(options.aperture)) {
-        if (typeof options.aperture === 'string') {
-            defaults.aperture = parseFloat(options.aperture)
+            this.aperture = parseFloat(match ? match[1] : '0')
         } else {
-            defaults.aperture = options.aperture
+            this.aperture = defaults.aperture
+        }
+
+        if (typeof cropFactor === 'number') {
+            this.cropFactor = cropFactor
+        } else {
+            this.cropFactor = defaults.cropFactor
+        }
+
+        // Optional properties
+        if (typeof id === 'string') {
+            this.id = id
+        }
+
+        if (typeof name === 'string') {
+            this.name = name
         }
     }
 
-    if (options.cropFactor && !isNaN(options.cropFactor)) {
-        defaults.cropFactor = options.cropFactor
+    setDefaults(options: Options) {
+        if (typeof options !== 'object' || !options) {
+            return
+        }
+
+        if (options.focalLength && !isNaN(options.focalLength)) {
+            defaults.focalLength = options.focalLength
+        }
+
+        if (options.aperture && !isNaN(options.aperture)) {
+            if (typeof options.aperture === 'string') {
+                defaults.aperture = parseFloat(options.aperture)
+            } else {
+                defaults.aperture = options.aperture
+            }
+        }
+
+        if (options.cropFactor && !isNaN(options.cropFactor)) {
+            defaults.cropFactor = options.cropFactor
+        }
+
+        if (options.distance && !isNaN(options.distance)) {
+            defaults.distance = options.distance
+        }
     }
 
-    if (options.distance && !isNaN(options.distance)) {
-        defaults.distance = options.distance
+    getResult(distance: number) {
+        if (isNaN(distance)) {
+            distance = defaults.distance
+        } else if (typeof distance === 'string') {
+            distance = parseFloat(distance)
+        }
+
+        return _calculate(this.focalLength, this.aperture, this.cropFactor, distance)
     }
-}
-
-DoF.prototype.getResult = function _getResult(distance: number) {
-    // const this = this
-
-    if (isNaN(distance)) {
-        distance = defaults.distance
-    } else if (typeof distance === 'string') {
-        distance = parseFloat(distance)
-    }
-
-    return _calculate(this.focalLength, this.aperture, this.cropFactor, distance)
 }
 
 export default DoF
