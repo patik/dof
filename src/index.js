ffc9c6f7eab236a77aee585f59c841741b99a169
Craig Patik <craig@patik.com>
2022-11-27 15:13:05 +0100

    add tests

--- src/index.js
+++ src/index.js
@@ -1,92 +1,78 @@
-(function (global, name, definition) {
-    // Require
-    if (typeof define === 'function' && define.amd) {
-        define([], definition);
-    }
-    // CommonJS
-    else if (typeof module !== 'undefined' && module.exports) {
-        module.exports = name;
-    }
-    // Fall back to a global variable
-    else {
-        global[name] = definition();
-    }
-}(this, 'DoF', function () {
+var DoF = (function () {
     var defaults = {
-            focalLength: 35, // Number, in millimeters. This must be the actual focal length, not the 35mm equivalent value
-            aperture: 2,     // String in the format `"f/2.5"`, or the float value `2.5`
-            cropFactor: 1,   // Sensor crop factor (compared to full frame; 2 = a half-size sensor)
-            distance: 20     // Distance to the subject (feet)
-        };
+        focalLength: 35, // Number, in millimeters. This must be the actual focal length, not the 35mm equivalent value
+        aperture: 2, // String in the format `"f/2.5"`, or the float value `2.5`
+        cropFactor: 1, // Sensor crop factor (compared to full frame; 2 = a half-size sensor)
+        distance: 20, // Distance to the subject (feet)
+    }
 
-    var apertureRegex = /^f\/(\d+(?:\.\d+)?)$/;
+    var apertureRegex = /^f\/(\d+(?:\.\d+)?)$/
 
     var _feetToFloat = function _feetToFloat(dist) {
-            var parts;
-            var feet;
-            var inches;
+        var parts
+        var feet
+        var inches
 
-            if (/^(\d+(?:\.\d+)?)\'\s+(\d+(?:\.\d+)?)\"$/.test(dist)) {
-                parts = /^(\d+(?:\.\d+)?)\'\s+(\d+(?:\.\d+)?)\"$/.exec(dist);
-                feet = parseFloat(parts[1]);
-                inches = parseFloat(parts[2]);
+        if (/^(\d+(?:\.\d+)?)\'\s+(\d+(?:\.\d+)?)\"$/.test(dist)) {
+            parts = /^(\d+(?:\.\d+)?)\'\s+(\d+(?:\.\d+)?)\"$/.exec(dist)
+            feet = parseFloat(parts[1])
+            inches = parseFloat(parts[2])
 
-                return parseFloat(feet + (inches / 12));
-            }
+            return parseFloat(feet + inches / 12)
+        }
 
-            return dist;
-        };
+        return dist
+    }
 
     var _calculate = function _calculate(focalLength, aperture, cropFactor, distance) {
-        var result = {};
-        var hf;
-        var near;
-        var far;
-        var dof;
-        var dofFeet;
+        var result = {}
+        var hf
+        var near
+        var far
+        var dof
+        var dofFeet
 
         // Convert to millimeters
-        distance = distance * 12 * 25.4;
+        distance = distance * 12 * 25.4
 
         // Get 35mm-equivalent focal length
-        result.focalLengthEquiv = _decimalAdjust(cropFactor * focalLength);
+        result.focalLengthEquiv = _decimalAdjust(cropFactor * focalLength)
 
         // Convert sensor crop factor to a multiplier
-        cropFactor = 1 / cropFactor;
+        cropFactor = 1 / cropFactor
 
-        result.coc = Math.round(0.03 * cropFactor * 1000) / 1000;
-        hf = Math.pow(focalLength, 2) / (aperture * result.coc) + (focalLength * 1.0);
+        result.coc = Math.round(0.03 * cropFactor * 1000) / 1000
+        hf = Math.pow(focalLength, 2) / (aperture * result.coc) + focalLength * 1.0
 
-        near = (distance * (hf - focalLength)) / (hf + distance + (2 * focalLength));
-        far = (distance * (hf - focalLength)) / (hf - distance);
+        near = (distance * (hf - focalLength)) / (hf + distance + 2 * focalLength)
+        far = (distance * (hf - focalLength)) / (hf - distance)
 
         if (far <= 0) {
-            far = Infinity;
-            dof = Infinity;
-        }
-        else {
-            dof = far - near;
+            far = Infinity
+            dof = Infinity
+        } else {
+            dof = far - near
         }
 
         // Gather all values
-        dofFeet = _mmToFeet(dof);
+        dofFeet = _mmToFeet(dof)
         result.toString = function () {
-            return dofFeet;
-        };
+            return dofFeet
+        }
 
-        result.toString.dof = dofFeet;
-        result.dof = _feetToFloat(result.toString.dof);
-        result.toString.eighthDof = _mmToFeet(dof / 8);
-        result.eighthDof = _feetToFloat(result.toString.eighthDof);
-        result.toString.hf = _mmToFeet(hf);
-        result.hf = _feetToFloat(result.toString.hf);
-        result.toString.near = _mmToFeet(near);
-        result.near = _feetToFloat(result.toString.near);
-        result.toString.far = _mmToFeet(far);
-        result.far = _feetToFloat(result.toString.far);
+        result.toString.dof = dofFeet
+        result.dof = _feetToFloat(result.toString.dof)
+        result.toString.eighthDof = _mmToFeet(dof / 8)
+        result.eighthDof = _feetToFloat(result.toString.eighthDof)
+        result.toString.hf = _mmToFeet(hf)
+        result.hf = _feetToFloat(result.toString.hf)
+        result.toString.near = _mmToFeet(near)
+        result.near = _feetToFloat(result.toString.near)
+        result.toString.far = _mmToFeet(far)
+        result.far = _feetToFloat(result.toString.far)
 
-        return result;
-    };
+        return result
+    }
 
     /**
      * Convert millimeters to decimal feet and inches
@@ -96,15 +82,14 @@
      */
     var _mmToFeet = function _mmToFeet(dist) {
         // Convert millimeters to inches
-        dist = dist / 25.4;
+        dist = dist / 25.4
 
         if (dist === Infinity) {
-            return Infinity;
+            return Infinity
+        } else {
+            return Math.floor(dist / 12) + "' " + (dist % 12).toFixed(1) + '"'
         }
-        else {
-            return Math.floor(dist / 12) + "' " + (dist % 12).toFixed(1) + '"';
-        }
-    };
+    }
 
     /**
      * Decimal adjustment of a number.
@@ -114,24 +99,24 @@
      * @returns {Number}            The adjusted value
      */
     var _decimalAdjust = function _decimalAdjust(value) {
-        var exp = -1; // The exponent (the 10 logarithm of the adjustment base)
+        var exp = -1 // The exponent (the 10 logarithm of the adjustment base)
 
-        value = +value;
+        value = +value
 
         // If the value is not a number or the exp is not an integer...
         if (isNaN(value)) {
-            return NaN;
+            return NaN
         }
 
         // Shift
-        value = value.toString().split('e');
-        value = Math.round(+(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp)));
+        value = value.toString().split('e')
+        value = Math.round(+(value[0] + 'e' + (value[1] ? +value[1] - exp : -exp)))
 
         // Shift back
-        value = value.toString().split('e');
+        value = value.toString().split('e')
 
-        return +(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp));
-    };
+        return +(value[0] + 'e' + (value[1] ? +value[1] + exp : exp))
+    }
 
     /**
      * Lens constructor
@@ -144,82 +129,78 @@
      */
     var DoF = function (focalLength, aperture, cropFactor, id, name) {
         if (typeof focalLength === 'string' && focalLength.length) {
-            focalLength = parseFloat(focalLength);
+            focalLength = parseFloat(focalLength)
         }
 
         if (typeof focalLength === 'number') {
-            this.focalLength = focalLength;
-        }
-        else {
-            this.focalLength = defaults.focalLength;
+            this.focalLength = focalLength
+        } else {
+            this.focalLength = defaults.focalLength
         }
 
         if (typeof aperture === 'number') {
-            this.aperture = aperture;
-        }
-        else if (typeof aperture === 'string' && apertureRegex.test(aperture)) {
-            this.aperture = parseFloat(apertureRegex.exec(aperture)[1]);
-        }
-        else {
-            this.aperture = defaults.aperture;
+            this.aperture = aperture
+        } else if (typeof aperture === 'string' && apertureRegex.test(aperture)) {
+            this.aperture = parseFloat(apertureRegex.exec(aperture)[1])
+        } else {
+            this.aperture = defaults.aperture
         }
 
         if (typeof cropFactor === 'number') {
-            this.cropFactor = cropFactor;
-        }
-        else {
-            this.cropFactor = defaults.cropFactor;
+            this.cropFactor = cropFactor
+        } else {
+            this.cropFactor = defaults.cropFactor
         }
 
         // Optional properties
         if (typeof id !== 'undefined') {
-            this.id = id;
+            this.id = id
         }
 
         if (typeof name !== 'undefined') {
-            this.name = name;
+            this.name = name
         }
-    };
+    }
 
     DoF.setDefaults = function _setDefaults(options) {
         if (typeof options !== 'object' || !options) {
-            return;
+            return
         }
 
         if (options.focalLength && !isNaN(options.focalLength)) {
-            defaults.focalLength = parseFloat(options.focalLength);
+            defaults.focalLength = parseFloat(options.focalLength)
         }
 
         if (options.aperture && !isNaN(options.aperture)) {
             if (typeof options.aperture === 'string') {
-                defaults.aperture = parseFloat(options.aperture);
-            }
-            else {
-                defaults.aperture = options.aperture;
+                defaults.aperture = parseFloat(options.aperture)
+            } else {
+                defaults.aperture = options.aperture
             }
         }
 
         if (options.cropFactor && !isNaN(options.cropFactor)) {
-            defaults.cropFactor = parseFloat(options.cropFactor);
+            defaults.cropFactor = parseFloat(options.cropFactor)
         }
 
         if (options.distance && !isNaN(options.distance)) {
-            defaults.distance = parseFloat(options.distance);
+            defaults.distance = parseFloat(options.distance)
         }
-    };
+    }
 
     DoF.prototype.getResult = function _getResult(distance) {
-        var _this = this;
+        var _this = this
 
         if (isNaN(distance)) {
-            distance = defaults.distance;
-        }
-        else if (typeof distance === 'string') {
-            distance = parseFloat(distance);
+            distance = defaults.distance
+        } else if (typeof distance === 'string') {
+            distance = parseFloat(distance)
         }
 
-        return _calculate(_this.focalLength, _this.aperture, _this.cropFactor, distance);
-    };
+        return _calculate(_this.focalLength, _this.aperture, _this.cropFactor, distance)
+    }
 
-    return DoF;
-}));
+    return DoF
+})()
+
+module.exports = DoF
---
+++ src/index.test.js
@@ -0,0 +1,95 @@
+var DoF = require('./index')
+
+describe('Module basics', () => {
+    afterEach(() => {
+        // Reset defaults
+        DoF.setDefaults({ focalLength: 35, cropFactor: 1, aperture: 2 })
+    })
+
+    test('constructor will create an object with the default settings', () => {
+        var lens = new DoF()
+
+        expect(lens.focalLength).toBe(35)
+        expect(lens.aperture).toBe(2)
+        expect(lens.cropFactor).toBe(1)
+    })
+
+    test('constructor will create an object with the provided settings', () => {
+        var lens = new DoF(40, 'f/2.5', 1.62, 1234, 'My Lens')
+
+        expect(lens.focalLength).toBe(40)
+        expect(lens.aperture).toBe(2.5)
+        expect(lens.cropFactor).toBe(1.62)
+        expect(lens.name).toBe('My Lens')
+        expect(lens.id).toBe(1234)
+    })
+
+    test('changes to default settings are retained', () => {
+        // First, create a lens with the original defaults
+        var lens1 = new DoF()
+
+        // Then change the defaults
+        DoF.setDefaults({ focalLength: 40, cropFactor: 1.62, aperture: 3.5 })
+
+        // Make a new lens, which should receive the new defaults
+        var lens2 = new DoF()
+
+        expect(lens1.focalLength).toBe(35)
+        expect(lens1.aperture).toBe(2)
+        expect(lens1.cropFactor).toBe(1)
+
+        expect(lens2.focalLength).toBe(40)
+        expect(lens2.aperture).toBe(3.5)
+        expect(lens2.cropFactor).toBe(1.62)
+    })
+})
+
+describe('Calculating the depth of field', () => {
+    test('with default settings', () => {
+        var lens = new DoF()
+        var result = lens.getResult()
+
+        expect(result.dof).toBe(13.1)
+        expect(result.dof.toString()).toBe('13.1')
+        expect(result.toString()).toBe('13\' 1.2"')
+
+        expect(result.eighthDof).toBe(1.6416666666666666)
+        expect(result.eighthDof.toString()).toBe('1.6416666666666666')
+
+        expect(result.hf).toBe(67.1)
+        expect(result.hf.toString()).toBe('67.1')
+
+        expect(result.near).toBe(15.341666666666667)
+        expect(result.near.toString()).toBe('15.341666666666667')
+
+        expect(result.far).toBe(28.441666666666666)
+        expect(result.far.toString()).toBe('28.441666666666666')
+
+        expect(result.coc).toBe(0.03)
+        expect(result.coc.toString()).toBe('0.03')
+    })
+
+    test('with a specific distance value', () => {
+        var lens = new DoF()
+        var result = lens.getResult(15)
+
+        expect(result.dof).toBe(7.083333333333333)
+        expect(result.dof.toString()).toBe('7.083333333333333')
+        expect(result.toString()).toBe('7\' 1.0"')
+
+        expect(result.eighthDof).toBe(0.8833333333333333)
+        expect(result.eighthDof.toString()).toBe('0.8833333333333333')
+
+        expect(result.hf).toBe(67.1)
+        expect(result.hf.toString()).toBe('67.1')
+
+        expect(result.near).toBe(12.208333333333334)
+        expect(result.near.toString()).toBe('12.208333333333334')
+
+        expect(result.far).toBe(19.283333333333335)
+        expect(result.far.toString()).toBe('19.283333333333335')
+
+        expect(result.coc).toBe(0.03)
+        expect(result.coc.toString()).toBe('0.03')
+    })
+})
