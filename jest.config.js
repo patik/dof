66f39d332a4d73216ec38457e756cac8cb723478
Craig Patik <craig@patik.com>
2022-12-10 14:11:43 +0100

    cleanup

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
     preset: 'ts-jest',
-    coverageProvider: 'babel',
     testEnvironment: 'node',
     rootDir: 'app',
 }
