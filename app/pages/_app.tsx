f614ab613c7b0f3652040609e5918d1a9bd94383
Craig Patik <craig@patik.com>
2022-12-11 15:08:47 +0100

    add MUI

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.css'
+import '@fontsource/open-sans'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <meta name="viewport" content="width=device-width,initial-scale=1,minimal-ui,viewport-fit=cover" />
+                <title>Depth of Field Calculator &amp; Comparison Tool for Camera Lenses</title>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
