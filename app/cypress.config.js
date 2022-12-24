0936d154f8126c3760734b7d970826b9a317e209
Craig Patik <craig@patik.com>
2022-12-24 15:06:54 -0500

    added test for Add Lens

--- app/cypress.config.js
+++ app/cypress.config.js
@@ -1,9 +1,16 @@
-module.exports = {
-    projectId: 'oxu623',
+import { defineConfig } from 'cypress'
 
+export default defineConfig({
+    projectId: 'oxu623',
     e2e: {
         setupNodeEvents(/* on, config */) {
             // implement node event listeners here
         },
     },
-}
+    component: {
+        devServer: {
+            framework: 'next',
+            bundler: 'webpack',
+        },
+    },
+})
---
+++ app/cypress/e2e/LensTable.cy.ts
@@ -0,0 +1,37 @@
+describe('LensTable', () => {
+    it('Updates the depth of field calculation when the inputs are changed', () => {
+        cy.visit('http://localhost:3000')
+
+        // Ensure units are set to metric so we know what numeric value to look for at the end
+        cy.get('button[title="Meters"]').click()
+
+        cy.get('[data-testid="focal-length-1"] input').focus().type('{selectall}').type('72')
+
+        cy.get('[data-testid="aperture-1"]').parent().click().get('ul > li[data-value="f/3.4"]').click()
+
+        cy.get('[data-testid="sensor-1"]').parent().click().get('ul > li[data-value="35mm"]').click()
+
+        cy.get('[data-testid="dof-1"]').should('have.text', '0.61')
+    })
+
+    it('The Add Lens button adds another lens', () => {
+        cy.visit('http://localhost:3000')
+
+        // Before adding the new lnes, get the ID if the latest existing lens, so we know what the next ID should be
+        cy.get('[data-testid^="name"]')
+            .last()
+            .invoke('get')
+            .then((lastLens) => {
+                // Find the most recent lens ID
+                const lastId = lastLens[0].dataset.testid.replace(/^name-/, '')
+
+                // Determine what the next lens ID should be
+                const nextId = `${Number(lastId) + 1}`
+
+                cy.get('button').contains('Add Lens').click()
+
+                // New lens should now be visible
+                cy.get(`[data-testid="name-${nextId}"]`).should('be.visible')
+            })
+    })
+})
