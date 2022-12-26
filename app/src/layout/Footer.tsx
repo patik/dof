a32d8364894a4acb675f96d7fd6f74aed3c64189
Craig Patik <craig@patik.com>
2022-12-26 16:45:59 -0500

    snackbar for permalink

--- app/src/layout/Footer.tsx
+++ app/src/layout/Footer.tsx
@@ -1,53 +1,13 @@
 import { Grid, Typography } from '@mui/material'
 import Link from 'next/link'
-import { useEffect, useState } from 'react'
-import { CopyToClipboard } from 'react-copy-to-clipboard'
-
-function usePermalink(isHomePage = false): string {
-    const [fullUrl, setFullUrl] = useState('')
-
-    useEffect(() => {
-        // Only relevant on the homepage
-        if (!isHomePage) {
-            return
-        }
-
-        const onHashChange = () => {
-            setFullUrl(window.location.href)
-        }
-
-        window.addEventListener('hashchange', onHashChange)
-
-        return () => window.removeEventListener('hashchange', onHashChange)
-    })
-
-    return fullUrl
-}
+import Permalink from '../components/Permalink'
 
 export default function Footer({ isHomePage }: { isHomePage?: boolean }) {
-    const fullUrl = usePermalink(isHomePage)
-
     return (
         <Grid container component="footer" mb={3} textAlign="center">
             <Grid item xs={4}>
                 <Typography variant="body2">
-                    {isHomePage ? (
-                        <CopyToClipboard text={fullUrl}>
-                            <a
-                                href={fullUrl}
-                                onClick={(e) => {
-                                    // Allow opening in a new tab
-                                    if (!e.metaKey) {
-                                        e.preventDefault()
-                                    }
-                                }}
-                            >
-                                Link to this comparison
-                            </a>
-                        </CopyToClipboard>
-                    ) : (
-                        <Link href="/">Back to the Tool</Link>
-                    )}
+                    {isHomePage ? <Permalink /> : <Link href="/">Back to the Tool</Link>}
                 </Typography>
             </Grid>
             <Grid item xs={4}>
