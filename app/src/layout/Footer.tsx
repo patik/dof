2edce36a79ab53e017c7dd5256f3040abe5ea261
Craig Patik <craig@patik.com>
2022-12-26 15:54:19 -0500

    permalink

--- app/src/layout/Footer.tsx
+++ app/src/layout/Footer.tsx
@@ -1,19 +1,52 @@
 import { Grid, Typography } from '@mui/material'
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+
+function usePermalink(isHomePage = false): string {
+    const [fullUrl, setFullUrl] = useState('')
+
+    useEffect(() => {
+        // Only relevant on the homepage
+        if (!isHomePage) {
+            return
+        }
+
+        const onHashChange = () => {
+            setFullUrl(window.location.href)
+        }
+
+        window.addEventListener('hashchange', onHashChange)
+
+        return () => window.removeEventListener('hashchange', onHashChange)
+    })
+
+    return fullUrl
+}
 
 export default function Footer({ isHomePage }: { isHomePage?: boolean }) {
+    const fullUrl = usePermalink(isHomePage)
+
     return (
         <Grid container component="footer" mb={3} textAlign="center">
             <Grid item xs={4}>
                 <Typography variant="body2">
                     {isHomePage ? (
-                        <Link href="/" className="comparison-link">
-                            Link to this comparison
-                        </Link>
+                        <CopyToClipboard text={fullUrl}>
+                            <a
+                                href={fullUrl}
+                                onClick={(e) => {
+                                    // Allow opening in a new tab
+                                    if (!e.metaKey) {
+                                        e.preventDefault()
+                                    }
+                                }}
+                            >
+                                Link to this comparison
+                            </a>
+                        </CopyToClipboard>
                     ) : (
-                        <Link href="/" className="comparison-link">
-                            Back to the Tool
-                        </Link>
+                        <Link href="/">Back to the Tool</Link>
                     )}
                 </Typography>
             </Grid>
