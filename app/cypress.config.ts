5a4b47873e75e9b793dcf2dded99dbe485b6db11
Craig Patik <craig@patik.com>
2023-01-02 12:32:28 +0100

    upgrade all dependencies

--- app/cypress.config.ts
+++ app/cypress.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig } from 'cypress'
-// import { config } from './package.json'
 
 export default defineConfig({
     projectId: 'oxu623',
     e2e: {
-        setupNodeEvents(/* on, config */) {
-            // implement node event listeners here
-        },
-        // baseUrl: `http://localhost:3000/${config.basePath}`,
         supportFile: false,
     },
     component: {
