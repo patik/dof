0d97dc74661170415e041e9a8d3a15c2f57af39c
Craig Patik <craig@patik.com>
2023-02-11 18:57:52 +0100

    export calculators

--- module/src/index.ts
+++ module/src/index.ts
@@ -3,3 +3,7 @@ export { createLensMaker } from './createLensMaker'
 export { preciseApertureMap as apertureMap, getApertureName, isApertureString } from './utilities/aperture'
 type DepthOfFieldDetails = DoFResult
 export type { DepthOfFieldDetails }
+
+export { calculateDepthOfField } from './calculators/calculateDepthOfField'
+export { calculateAperture } from './calculators/calculateAperture'
+export { calculateFocalLength } from './calculators/calculateFocalLength'
