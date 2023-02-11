1fdfa3612206cc2bcf1d34ba77e923758afc1be4
Craig Patik <craig@patik.com>
2023-02-11 17:28:14 +0100

    calculateAperture takes an object for input

--- module/src/utilities/calculateAperture.test.ts
+++ module/src/utilities/calculateAperture.test.ts
@@ -3,26 +3,54 @@ import { calculateAperture } from './calculateAperture'
 describe('Calculating the depth of field with calculateDepthOfField', () => {
     describe('metric units (meters)', () => {
         test('DoF 2.584690961719362 meters, 35mm, f/2, crop factor of 1, 5 meters', () => {
-            const result = calculateAperture(35, 1, 5, 2.584690961719362, 4.021931840567339, false)
+            const result = calculateAperture({
+                focalLength: 35,
+                cropFactor: 1,
+                distance: 5,
+                dof: 2.584690961719362,
+                nearLimit: 4.021931840567339,
+                imperialUnits: false,
+            })
 
             expect(result.aperture).toBe(2)
         })
 
         test('DoF 11.082093523926748 meters, 50mm, f/1.4, crop factor of 2, 25 meters', () => {
-            const result = calculateAperture(50, 2, 25, 11.082093523926748, 20.63203625607849, false)
+            const result = calculateAperture({
+                focalLength: 50,
+                cropFactor: 2,
+                distance: 25,
+                dof: 11.082093523926748,
+                nearLimit: 20.63203625607849,
+                imperialUnits: false,
+            })
 
             expect(result.aperture).toBe(1.4142139999999992)
         })
 
         test('DoF 7.851429380291624 meters, 28mm, f/5, crop factor of 3.02, 7 meters', () => {
             // Blackmagic Cine Cam sensor
-            const result = calculateAperture(28, 3.02, 7, 7.851429380291624, 4.833680315786585, false)
+            const result = calculateAperture({
+                focalLength: 28,
+                cropFactor: 3.02,
+                distance: 7,
+                dof: 7.851429380291624,
+                nearLimit: 4.833680315786585,
+                imperialUnits: false,
+            })
 
             expect(result.aperture).toBe(5.039684)
         })
 
         test('such that the far end of the range is infinity', () => {
-            const result = calculateAperture(24, 1, 5, Infinity, 0.9715025906735751, false)
+            const result = calculateAperture({
+                focalLength: 24,
+                cropFactor: 1,
+                distance: 5,
+                dof: Infinity,
+                nearLimit: 0.9715025906735751,
+                imperialUnits: false,
+            })
 
             expect(result.aperture).toBe(NaN)
         })
@@ -30,13 +58,27 @@ describe('Calculating the depth of field with calculateDepthOfField', () => {
 
     describe('imperial units (feet)', () => {
         test('DoF 7.012923816256398 feet, 35mm, f/2, crop factor of 1, 15 feet', () => {
-            const result = calculateAperture(35, 1, 15, 7.012923816256398, 12.27274548830499, true)
+            const result = calculateAperture({
+                focalLength: 35,
+                cropFactor: 1,
+                distance: 15,
+                dof: 7.012923816256398,
+                nearLimit: 12.27274548830499,
+                imperialUnits: true,
+            })
 
             expect(result.aperture).toBe(2.000000000000001)
         })
 
         test('DoF 12.634717336600715 feet, 55mm, f/3.2, crop factor of 2.7, 42 feet', () => {
-            const result = calculateAperture(55, 2.7, 42, 12.634717336600715, 36.61227928000945, true)
+            const result = calculateAperture({
+                focalLength: 55,
+                cropFactor: 2.7,
+                distance: 42,
+                dof: 12.634717336600715,
+                nearLimit: 36.61227928000945,
+                imperialUnits: true,
+            })
 
             expect(result.aperture).toBe(3.1748020000000055)
         })
--- module/src/utilities/calculateAperture.ts
+++ module/src/utilities/calculateAperture.ts
@@ -2,30 +2,29 @@ import { getApertureName } from './aperture'
 
 /**
  * Returns the aperture for a given lens' attributes and depth of field
- *
- * @param focalLength
- * @param aperture
- * @param cropFactor
- * @param distance
- * @param dof
- * @param near
- * @param imperialUnits
  */
-export function calculateAperture(
-    focalLength: number,
-    cropFactor: number,
-    distance: number,
-    dof: number,
-    near: number,
+export function calculateAperture({
+    focalLength,
+    cropFactor,
+    distance,
+    dof,
+    nearLimit,
+    imperialUnits,
+}: {
+    focalLength: number
+    cropFactor: number
+    distance: number
+    dof: number
+    nearLimit: number
     imperialUnits?: boolean
-): ApertureResult {
+}): ApertureResult {
     // e.g. 1 foot is 30.48% of 1 meter
     const unitMultiplier = imperialUnits ? 0.3048 : 1
 
     // Convert to millimeters
     const mmDist = distance * 1000 * unitMultiplier
     const mmDof = dof * 1000 * unitMultiplier
-    const mmNear = near * 1000 * unitMultiplier
+    const mmNear = nearLimit * 1000 * unitMultiplier
     const mmFar = mmNear + mmDof
     const mmHF = (-1 * mmDist * focalLength + mmDist * mmFar) / (-1 * mmDist + mmFar)
     const cropMultiplier = 1 / cropFactor
